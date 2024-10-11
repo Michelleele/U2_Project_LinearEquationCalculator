0f175e21825b681c4e500adfa2dcabbe9d55a22d
BT_2S14_27 <schools@gmail.com>
2024-10-11 11:03:23 -0400

    added comments

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,4 +1,6 @@
 public class LinearEquation {
+
+    //attributes
     private int xOne;
     private int yOne;
     private int xTwo;
@@ -10,6 +12,7 @@ public class LinearEquation {
     private String printedFractionSlope;
     private String printedYInt;
 
+    // constructor method that creates an object and initiate all the attributes located in this class and performs all the necessary calculations
     public LinearEquation(String userInputOne, String userInputTwo) {
         xOne = Integer.parseInt(userInputOne.substring(1, userInputOne.indexOf(",")));
         yOne = Integer.parseInt(userInputOne.substring(userInputOne.indexOf(",") + 1, userInputOne.length() - 1));
@@ -20,6 +23,7 @@ public class LinearEquation {
         calculations();
     }
 
+    // method to organize all the calculations that needs to be performed in their specific order
     public void calculations() {
         findSlopeDenominator();
         findSlopeNumerator();
@@ -29,38 +33,47 @@ public class LinearEquation {
         setPrintYInt();
     }
 
+    // finds the numerator of the slope of the line
     public void findSlopeNumerator() {
         slopeNumerator = yTwo - yOne;
     }
 
+    //finds the denominator of the slope of the line
     public void findSlopeDenominator() {
         slopeDenominator = xTwo - xOne;
     }
 
+    //finds the actual value of the slope of the line
     public void setCalcSlope() {
         calculationSlope = (double) slopeNumerator / slopeDenominator;
     }
 
+    //checks if the slope is one
     public boolean isSlopeOne() {
         return slopeNumerator == slopeDenominator;
     }
 
+    //checks if the slope is zero
     public boolean isYIntZero() {
         return yInt == 0;
     }
 
+    //checks if the slope is a whole number
     public boolean isSlopeWholeNumber() {
         return (slopeNumerator % slopeDenominator == 0);
     }
 
+    //checks if the parameter stored a negative value
     public boolean isNeg(double x) {
         return (x < 0);
     }
 
+    //finds the y intercept of the line
     public void findYInt() {
         yInt = yOne - (calculationSlope * xOne);
     }
 
+    //sets y intercept into a String and adding - in front if necessary
     public void setPrintYInt() {
         if (!isYIntZero()) {
             if (isNeg(yInt)) {
@@ -73,13 +86,15 @@ public class LinearEquation {
         }
     }
 
+    // evaluating whether x needs a negative sign in front of its String and returns one if needed
     public String setPosOrNeg(double x) {
         if (isNeg(x)) {
-            return " -";
+            return " - ";
         }
         return " ";
     }
 
+    //sets the numerator and denominator of the slope to positive values if both the numerator and denominator is a negative value
     public void setSlopeNumeratorDenominatorStandard() {
         if (isNeg(slopeNumerator) && isNeg(slopeDenominator)) {
             slopeNumerator *= -1;
@@ -87,11 +102,13 @@ public class LinearEquation {
         }
     }
 
+    // sets the slope of the line as a String stored in variable printedFractionSlope
     public void setFractionSlope() {
         setSlopeNumeratorDenominatorStandard();
         printedFractionSlope = setPosOrNeg(calculationSlope) + slopeNumerator + "/" + slopeDenominator;
     }
 
+    // sets up the linear equation of the line as a String
     public String findLinearEquation() {
         if (isSlopeWholeNumber()) {
             if (isSlopeOne()) {
@@ -104,20 +121,24 @@ public class LinearEquation {
         }
     }
 
+    //finds the distance between the two coordinates
     public double findDistance() {
         int a = xTwo - xOne;
         int b = yTwo - yOne;
         return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     }
 
+    //placed and organized all information regarding the two coordinates into a String
     public String toString() {
         return (" Slope of line:" + printedFractionSlope + "\n Y-intercept:" + printedYInt + "\n Slope intercept form: " + findLinearEquation() + "\n Distance between points: " + findDistance());
     }
 
+    //solves for the value of y using the linear equation and the x value given by user
     public double solveForY(double userX) {
         return ((calculationSlope * userX) + yInt);
     }
 
+    //placed and organized the resulting coordinate point from method solveForY() into a String
     public String printSolvedCoordinates(double userX) {
         return ("Solved coordinate point is: (" + userX + ", " + solveForY(userX) + ")");
     }
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,22 @@ class Main {
     public static void main(String[] args) {
         Scanner kyx = new Scanner(System.in);
 
+        //Asking the user for inputs of two coordinate points
         System.out.print("Enter your first Coordinate Point: ");
         String coordinateOne = kyx.nextLine();
         System.out.print("Enter your second Coordinate Point: ");
         String coordinateTwo = kyx.nextLine();
 
+        //creates a new object using user inputs
         LinearEquation linearOne = new LinearEquation(coordinateOne, coordinateTwo);
 
+        //prints out info on the two coordinate points
         System.out.print("---------------------------------------------------------\n" + linearOne + "\n---------------------------------------------------------\nEnter a third x-value: ");
+
+        //asks the user for an x a=value input
         double x = kyx.nextDouble();
+
+        //prints out the resulting coordinate point
         System.out.println("---------------------------------------------------------\n"+ linearOne.printSolvedCoordinates(x));
         }
     }
